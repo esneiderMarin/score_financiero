262b585aeb8745f51f1c306fe713c5d77a92eab5
Esneider Marín Zapata <esneider.m97@gmail.com>
2020-01-20 14:12:25 -0500

    Primer commit
    Se hace el commit inicial con la creacion de el proyecto diagnosticofinanciero en spring

---
+++ diagnosticofinanciero/src/main/java/com/suramericana/diagnosticofinanciero/DiagnosticofinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.suramericana.diagnosticofinanciero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticofinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticofinancieroApplication.class, args);
+	}
+
+}
---
+++ diagnosticofinanciero/src/test/java/com/suramericana/diagnosticofinanciero/DiagnosticofinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suramericana.diagnosticofinanciero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticofinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
